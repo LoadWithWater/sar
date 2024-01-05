eee217440e0c9a1f4893f3ff0a3236b99ff0ca3e
Kim <rlarhkdwo98@naver.com>
2024-01-05 23:26:00 +0900

    background 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import SARmain from "./img/SARmain.jpg";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      안녕하세요
+    <div style={{backgroundImage : `url(/SARmain.png)`}}>
+      <button>PLAY STEAM</button>
+      <button>PLAY NINTENDO</button>
+
+      <h1>FIGHT FOR FURVIVAL</h1>
+      64 animals enter. Only one gets to do the winner dance.
+      
     </div>
   );
 }
